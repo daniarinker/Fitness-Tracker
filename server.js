a99c8e5e58325e01ee7e26ab1f18a89038c25b64
Dania Rinker <daniarinker@gmail.com>
2021-02-27 18:09:19 -0800

    fixed server.js dupicate

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+// routes
+
+require("./routes/htmlRoutes")(app);
+
+app.use(require("./routes/api.js"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,19 +16,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false,
 });
-
-process.env.MONGODB_URI || "mongodb://localhost/fitness/workoutdb",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  };
-
-// routes
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
